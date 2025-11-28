92ba00b97b6cbcd4a193036bc7fbd9e35b50e9d0
Tran Minh Khoa <tranminhkhoa2k42k5@gmail.com>
2025-11-28 14:20:39 +0700

    feat: delete unrelated module

--- src/backend/test-service/src/main/java/com/project/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.project.exception;
-
-public class BadRequestException extends BaseException {
-    public BadRequestException(String message) {
-        super(400, message);
-    }
-}
--- src/backend/test-service/src/main/java/com/project/exception/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.project.exception;
-
-public class NotFoundException extends BaseException {
-    public NotFoundException(String message) {
-        super(404, message);
-    }
-}
